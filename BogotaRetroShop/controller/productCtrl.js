24e45db90da339a8d17dc97c2661e68596102d4a
dbellol <dbellol@unal.edu.co>
2023-10-15 16:23:56 -0500

    controllador productCtrl
    Add A Product
    Get  A Product

--- BogotaRetroShop/controller/productCtrl.js
+++ BogotaRetroShop/controller/productCtrl.js
@@ -8,4 +8,13 @@ const createProduct=asyncHandler(async(req,res)=>{
         throw new Error(error);
     }
 });
-module.exports={createProduct};
+const getProduct = asyncHandler(async(req,res)=>{
+    const{id}=req.params;
+    try{
+        const findProduct=await Product.findById(id);
+        res.json(findProduct);
+    }catch(error){
+        throw new Error(error);
+    }
+});
+module.exports={createProduct, getProduct};
