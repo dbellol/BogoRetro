7f776a99dde606af9ad3f28fdf9df8b07352ba98
dbellol <dbellol@unal.edu.co>
2023-10-17 11:16:08 -0500

    Comentarios en Utils

--- BogotaRetroShop/utils/cloudinary.js
+++ BogotaRetroShop/utils/cloudinary.js
@@ -1,10 +1,11 @@
 const cloudinary = require("cloudinary");
+/*Configuracion de cloudinary (donde se almacenaran las imagenes)*/
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.SECRET_KEY,
 });
-
+/*Carga de imagenes a cloudinary*/
 const cloudinaryUploadImg = async(fileToUploads)=>{
     return new Promise((resolve)=>{
         cloudinary.uploader.upload(fileToUploads, (result)=>{
--- BogotaRetroShop/utils/validateMongodbId.js
+++ BogotaRetroShop/utils/validateMongodbId.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+/*Validación de colecciones y registros en mongo DB*/
 const validateMongoId = (id) => {
   const isValid = mongoose.Types.ObjectId.isValid(id);
   if (!isValid) throw new Error("This id is not valid or not Found");
