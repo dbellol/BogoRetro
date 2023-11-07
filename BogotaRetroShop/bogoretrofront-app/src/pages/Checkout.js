06d46b01f7936419a32e42fff77024854a41491e
dbellol <dbellol@unal.edu.co>
2023-11-06 20:53:36 -0500

    Listo carrito, se empieza pasarela de pago
    F

---
+++ BogotaRetroShop/bogoretrofront-app/src/pages/Checkout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <>
+        <div className='checkout-wrapper'>
+            <div className='container-xxl'>
+                <div className='row'>
+                    <div className='col-7'>
+                        <div className='col-5'>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </>
+  )
+}
+
+export default Checkout
