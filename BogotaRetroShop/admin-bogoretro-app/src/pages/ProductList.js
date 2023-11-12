195c6e2fefc7c6d08579279495158e3799a11dc4
dbellol <dbellol@unal.edu.co>
2023-11-11 20:26:29 -0500

    Lista de productos

--- BogotaRetroShop/admin-bogoretro-app/src/pages/ProductList.js
+++ BogotaRetroShop/admin-bogoretro-app/src/pages/ProductList.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from 'antd';
+import {useDispatch, useSelector} from 'react-redux';
 import {BiEdit} from 'react-icons/bi';
 import {AiFillDelete} from 'react-icons/ai';
+import { getProducts } from '../features/product/productSlice';
 const columns = [
   {
     title: 'NSerial',
@@ -9,27 +11,53 @@ const columns = [
   },
   {
     title: 'Nombre',
-    dataIndex: 'name',
+    dataIndex: 'title',
   },
   {
-    title: 'Producto',
-    dataIndex: 'product',
+    title: 'Descripcion',
+    dataIndex: 'description',
   },
   {
-    title: 'Estado',
-    dataIndex: 'status',
+    title: 'Precio',
+    dataIndex: 'price',
+  },
+  {
+    title: 'Edad',
+    dataIndex: 'age',
+  },
+  {
+    title: 'Categoría',
+    dataIndex: 'category',
+  },
+  {
+    title: 'Marca',
+    dataIndex: 'brand',
+  },
+  {
+    title: 'Cantidad',
+    dataIndex: 'quantity',
   },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Edward King ${i}`,
-    product: 32,
-    status: `London, Park Lane no. ${i}`,
-  });
-}
 const Productlist = () => {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getProducts());
+  },[]);
+  const productState = useSelector((state)=>state.product.products);
+  const data1 = [];
+  for (let i = 0; i < productState.length; i++) {
+    data1.push({
+      key: i+1,
+      title: productState[i].title,
+      description: productState[i].description,
+      price: productState[i].price,
+      age: productState[i].age,
+      category: productState[i].category,
+      brand: productState[i].brand,
+      quantity: productState[i].quantity,
+    });
+  }
+  console.log(data1);
   return (
     <div>
         <h3 className='mb-4  title'>Lista de productos </h3>
